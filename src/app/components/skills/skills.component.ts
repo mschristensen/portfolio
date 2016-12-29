1dc84148ed3cad2a8c29c7473d1ba9313b60ab38
Mike Christensen <mike_christensen@btinternet.com>
2016-12-29 16:14:20 +0000

    skills section implemented

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -12,4 +12,24 @@ export class SkillsComponent implements OnInit {
   ngOnInit() {
   }
 
+  skills = [
+    { name: 'HTML5', url: 'assets/images/skills/html5.svg' },
+    { name: 'CSS3', url: 'assets/images/skills/css3.svg' },
+    { name: 'Less', url: 'assets/images/skills/less.svg' },
+    { name: 'JavaScript', url: 'assets/images/skills/javascript.svg' },
+    { name: 'AngularJS', url: 'assets/images/skills/angular.svg' },
+    { name: 'jQuery', url: 'assets/images/skills/jQuery.svg' },
+    { name: 'Node.js', url: 'assets/images/skills/node.svg' },
+    { name: 'MongoDB', url: 'assets/images/skills/mongodb.svg' },
+    { name: 'PostgreSQL', url: 'assets/images/skills/postgresql.svg' },
+    { name: 'AWS', url: 'assets/images/skills/aws.svg' },
+    { name: 'Heroku', url: 'assets/images/skills/heroku.svg' },
+    { name: 'Python', url: 'assets/images/skills/python.svg' },
+    { name: 'Grunt', url: 'assets/images/skills/grunt.svg' },
+    { name: 'Linux', url: 'assets/images/skills/linux.svg' },
+    { name: 'Windows', url: 'assets/images/skills/windows.svg' },
+    { name: 'Git', url: 'assets/images/skills/git.svg' },
+    { name: 'GIMP', url: 'assets/images/skills/gimp.svg' },
+    { name: 'Inkscape', url: 'assets/images/skills/inkscape.svg' }
+  ];
 }
